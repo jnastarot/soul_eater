64098072d1baab76ae11cb3bac49533df1f23184
jnastarot <jnastarot@yandex.ru>
2019-05-28 00:47:36 +0300

    init env

--- soul_eater/stdafx.h
+++ soul_eater/stdafx.h
@@ -1,14 +1,32 @@
-﻿// stdafx.h: включаемый файл для стандартных системных включаемых файлов
-// или включаемых файлов для конкретного проекта, которые часто используются, но
-// нечасто изменяются
-//
+﻿#pragma once
 
-#pragma once
+#define WIN32_LEAN_AND_MEAN
 
-#include "targetver.h"
+#include <algorithm>
+#include <cstdint>
+#include <vector>
+#include <list>
+#include <math.h>
+#include <map>
+#include <set>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <sstream>
+#include <stdarg.h> 
+#include <iostream>
+#include <iostream>
 
-#define WIN32_LEAN_AND_MEAN             // Исключите редко используемые компоненты из заголовков Windows
+#include <locale.h>
+
+using namespace std;
 
 
+#pragma comment(lib, "capstone.lib")
+#pragma comment(lib, "enma_pe.lib")
+#pragma comment(lib, "fukutasm.lib")
 
-// установите здесь ссылки на дополнительные заголовки, требующиеся для программы
+#include "enma_pe\enma_pe\enma_pe.h"
+#include "capstone\include\capstone\capstone.h"
+#include "fukutasm\fukutasm\fukutasm.h"
